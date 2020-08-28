9affabd95a5418443847c265f09ceb7f86c9aeda
Danieljvdw <daniel.j.v.d.w@gmail.com>
2020-08-28 23:48:16 +0100

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace FileTag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgvTags = new System.Windows.Forms.DataGridView();
+            this.clmTags = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDirectory = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dgvFiles = new System.Windows.Forms.DataGridView();
+            this.clmFile = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTags)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tag";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // dgvTags
+            // 
+            this.dgvTags.AllowUserToAddRows = false;
+            this.dgvTags.AllowUserToDeleteRows = false;
+            this.dgvTags.AllowUserToResizeColumns = false;
+            this.dgvTags.AllowUserToResizeRows = false;
+            this.dgvTags.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvTags.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTags.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmTags});
+            this.tableLayoutPanel1.SetColumnSpan(this.dgvTags, 3);
+            this.dgvTags.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvTags.Location = new System.Drawing.Point(3, 46);
+            this.dgvTags.Name = "dgvTags";
+            this.dgvTags.ReadOnly = true;
+            this.dgvTags.RowHeadersVisible = false;
+            this.dgvTags.RowHeadersWidth = 62;
+            this.dgvTags.RowTemplate.Height = 28;
+            this.dgvTags.Size = new System.Drawing.Size(291, 1091);
+            this.dgvTags.TabIndex = 3;
+            this.dgvTags.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvTags_CellDoubleClick);
+            this.dgvTags.SelectionChanged += new System.EventHandler(this.DgvTags_SelectionChanged);
+            // 
+            // clmTags
+            // 
+            this.clmTags.HeaderText = "Tag";
+            this.clmTags.MinimumWidth = 8;
+            this.clmTags.Name = "clmTags";
+            this.clmTags.ReadOnly = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Directory";
+            // 
+            // lblDirectory
+            // 
+            this.lblDirectory.AutoSize = true;
+            this.lblDirectory.Location = new System.Drawing.Point(378, 0);
+            this.lblDirectory.Name = "lblDirectory";
+            this.lblDirectory.Size = new System.Drawing.Size(17, 20);
+            this.lblDirectory.TabIndex = 5;
+            this.lblDirectory.Text = "\\\\";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(401, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 37);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // dgvFiles
+            // 
+            this.dgvFiles.AllowUserToAddRows = false;
+            this.dgvFiles.AllowUserToDeleteRows = false;
+            this.dgvFiles.AllowUserToResizeColumns = false;
+            this.dgvFiles.AllowUserToResizeRows = false;
+            this.dgvFiles.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmFile});
+            this.tableLayoutPanel1.SetColumnSpan(this.dgvFiles, 3);
+            this.dgvFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvFiles.Location = new System.Drawing.Point(300, 46);
+            this.dgvFiles.Name = "dgvFiles";
+            this.dgvFiles.ReadOnly = true;
+            this.dgvFiles.RowHeadersVisible = false;
+            this.dgvFiles.RowHeadersWidth = 62;
+            this.dgvFiles.RowTemplate.Height = 28;
+            this.dgvFiles.Size = new System.Drawing.Size(923, 1091);
+            this.dgvFiles.TabIndex = 7;
+            this.dgvFiles.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvFiles_CellClick);
+            // 
+            // clmFile
+            // 
+            this.clmFile.HeaderText = "File";
+            this.clmFile.MinimumWidth = 8;
+            this.clmFile.Name = "clmFile";
+            this.clmFile.ReadOnly = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dgvFiles, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dgvTags, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblDirectory, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 3, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1226, 1140);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1226, 1140);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "FileTag";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTags)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvTags;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dgvFiles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmFile;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblDirectory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmTags;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace FileTag
+{
+    public partial class Form1 : Form
+    {
+        string fileTagsFileName = Directory.GetCurrentDirectory() + "\\fileTags.xml";
+        string taggedFilesFileName = Directory.GetCurrentDirectory() + "\\taggedFiles.xml";
+
+        List<taggedFile> taggedFiles = new List<taggedFile>();
+        taggedFile currentTaggedFile = new taggedFile();
+        List<string> tags = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            loadTags();
+            loadTaggedFiles();
+            loadFiles(Properties.Settings.Default.Path);
+        }
+
+#if false
+        private void deleteDuplicated(object sender, EventArgs e)
+        {
+            string[] duplicate = File.ReadAllLines("C:\\Users\\danie\\Documents\\duplicate.txt");
+
+            foreach(string s in duplicate)
+            {
+                if(s.Contains("cont") || s.StartsWith("-"))
+                {
+
+                }
+                else
+                {
+                    string[] tokens = s.Split('\t');
+                    string path = tokens[1] + "\\" + tokens[0];
+
+                    File.Delete(path);
+
+                    Console.WriteLine("Will delete: " + path);
+                }
+            }
+        }
+#endif
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                Properties.Settings.Default.Path = folderBrowserDialog.SelectedPath;
+                Properties.Settings.Default.Save();
+
+                loadFiles(Properties.Settings.Default.Path);
+            }
+        }
+
+        private void loadFiles(string path)
+        {
+            if (!Directory.Exists(path))
+                return;
+
+            lblDirectory.Text = path;
+
+            dgvFiles.Rows.Clear();
+
+            string[] files = Directory.GetFiles(path);
+            foreach (string s in files)
+            {
+                dgvFiles.Rows.Add(s.Substring(path.Length + 1));
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            saveTag(textBox1.Text);
+        }
+
+        private void DgvFiles_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            saveTaggedFile();
+            loadTaggedFile((string)dgvFiles.Rows[e.RowIndex].Cells[0].Value);
+        }
+
+        private void DgvTags_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            toggleTag((string)dgvTags.Rows[e.RowIndex].Cells[0].Value);
+        }
+
+        private void loadTags()
+        {
+            dgvTags.Rows.Clear();
+
+#if false
+            DataSet ds = new DataSet();
+
+            if (!File.Exists(fileTagsFileName))
+                return;
+
+            ds.ReadXml(fileTagsFileName);
+
+            foreach (DataRow d in ds.Tables[0].Rows)
+            {
+                dgvTags.Rows.Add(d.ItemArray);
+            }
+#else
+            if (!File.Exists(fileTagsFileName))
+                return;
+
+            tags = xml.ReadFromXmlFile<List<string>>(fileTagsFileName);
+
+            foreach (string tag in tags)
+            {
+                dgvTags.Rows.Add(tag);
+            }
+#endif
+        }
+
+        private void saveTag(string tag)
+        {
+#if false
+            dgvTags.Rows.Add(tag);
+
+            DataTable dt = new DataTable();
+            dt.TableName = "Tags";
+
+            for (int i = 0; i < dgvTags.Columns.Count; i++)
+            {
+                string headerText = dgvTags.Columns[i].HeaderText;
+                headerText = Regex.Replace(headerText, "[-/, ]", "_");
+
+                DataColumn column = new DataColumn(headerText);
+                dt.Columns.Add(column);
+            }
+
+            foreach (DataGridViewRow row in dgvTags.Rows)
+            {
+                object[] cellValues = new object[dgvTags.Columns.Count];
+
+                for (int i = 0; i < row.Cells.Count; i++)
+                {
+                    cellValues[i] = row.Cells[i].Value;
+                }
+
+                if (!row.IsNewRow)
+                    dt.Rows.Add(cellValues);
+            }
+
+            dt.WriteXml(fileTagsFileName);
+#else
+
+#endif
+            tags.Add(tag);
+
+            xml.WriteToXmlFile<List<string>>(fileTagsFileName, tags);
+
+            loadTags();
+            updateTags();
+        }
+
+        private void loadTaggedFiles()
+        {
+            if (!File.Exists(taggedFilesFileName))
+                return;
+
+            taggedFiles = xml.ReadFromXmlFile<List<taggedFile>>(taggedFilesFileName);
+        }
+
+        private void loadTaggedFile(string filename)
+        {
+            bool fileExists = false;
+
+            foreach(taggedFile taggedFile in taggedFiles)
+            {
+                if(taggedFile.file == filename)
+                {
+                    currentTaggedFile = taggedFile;
+                    fileExists = true;
+                    break;
+                }
+            }
+
+            if (!fileExists)
+            {
+                currentTaggedFile = new taggedFile();
+                currentTaggedFile.file = filename;
+                taggedFiles.Add(currentTaggedFile);
+            }
+
+            updateTags();
+        }
+
+        private void saveTaggedFile()
+        {
+            int filePosition = 0;
+            bool fileExists = false;
+
+            foreach (taggedFile taggedFile in taggedFiles)
+            {
+                if (taggedFile.file == currentTaggedFile.file)
+                {
+                    currentTaggedFile = taggedFile;
+                    fileExists = true;
+                    break;
+                }
+
+                filePosition++;
+            }
+
+            taggedFiles.Insert(filePosition, currentTaggedFile);
+            if (fileExists)
+            {
+                taggedFiles.RemoveAt(filePosition + 1);
+            }
+
+#if flase
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(taggedFile));
+            
+            using (var stream = File.OpenWrite(taggedFilesFileName))
+            {
+                xmlSerializer.Serialize(stream, taggedFiles);
+            }
+#else
+            xml.WriteToXmlFile<List<taggedFile>>(taggedFilesFileName, taggedFiles);
+#endif
+        }
+
+        private void toggleTag(string tag)
+        {
+            bool isTagged = false;
+            int tagPosition = 0;
+
+            foreach(string t in currentTaggedFile.tags)
+            {
+                if(t == tag)
+                {
+                    isTagged = true;
+                    break;
+                }
+
+                tagPosition++;
+            }
+
+            if(isTagged)
+            {
+                currentTaggedFile.tags.RemoveAt(tagPosition);
+            }
+            else
+            {
+                currentTaggedFile.tags.Add(tag);
+            }
+
+            updateTags();
+        }
+
+        private void updateTags()
+        {
+            foreach (DataGridViewRow tagRow in dgvTags.Rows)
+            {
+                bool tagToggled = false;
+
+                foreach (string tag in currentTaggedFile.tags)
+                {
+                    if (tag == (string)tagRow.Cells[0].Value)
+                    {
+                        tagToggled = true;
+                        break;
+                    }
+                }
+
+                if (tagToggled)
+                {
+                    tagRow.Cells[0].Style.BackColor = Color.Green;
+                }
+                else
+                {
+                    tagRow.Cells[0].Style.BackColor = Color.White;
+                }
+            }
+        }
+
+        private void DgvTags_SelectionChanged(object sender, EventArgs e)
+        {
+            dgvTags.ClearSelection();
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                Button1_Click(sender, e);
+                textBox1.Focus();
+                textBox1.SelectAll();
+            }
+        }
+    }
+
+    public class taggedFile
+    {
+        
+        public string file;
+        public List<string> tags;
+
+        public taggedFile()
+        {
+            file = "";
+            tags = new List<string>();
+        }
+    }
+}
